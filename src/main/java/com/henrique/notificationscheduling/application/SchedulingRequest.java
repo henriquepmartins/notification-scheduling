1b1db0da7d4c1831cee9b151a66700cf06113ac8
henrique <henriquepermartins@gmail.com>
2025-06-05 17:38:18 -0300

    feat: add unit tests for SchedulingController and ensure proper handling of scheduling requests

--- src/main/java/com/henrique/notificationscheduling/application/SchedulingRequest.java
+++ src/main/java/com/henrique/notificationscheduling/application/SchedulingRequest.java
@@ -7,5 +7,5 @@ import java.time.LocalDateTime;
 public record SchedulingRequest(String recipientMail, String recipientPhone, String message,
                                 @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
                                 LocalDateTime sentDate) {
-
+    
 }
